a776c9c6dd4c7c16974f67ec4f993121aebab45e
Huan LI <zixia@zixia.net>
2018-08-04 01:51:48 +0800

    upgrade rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 export default {
-  entry: 'dist/index.js',
-  dest: 'bundles/chinese-whispers.es6.umd.js',
-  sourceMap: true,
-  format: 'umd',
-  moduleName: 'window',
+  input: 'dist/index.js',
+  output: {
+    file: 'bundles/chinese-whispers.es6.umd.js',
+    format: 'umd',
+  },
+  sourcemap: true,
+  name: 'window',
   banner: '/* chinese-whisper version ' + require('./package.json').version + ' */',
   footer: '/* https://git.io/zixia/ */'
 }
