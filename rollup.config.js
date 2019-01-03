67bbbbc17b7b350e6d46a806541fa8657e12c6e8
Huan LI <zixia@zixia.net>
2019-01-03 18:57:18 +0800

    follow rollup config changes

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ export default {
   output: {
     file: 'bundles/chinese-whispers.es6.umd.js',
     format: 'umd',
+    name: 'window',
+    sourcemap: true,
+    banner: '/* chinese-whisper version ' + require('./package.json').version + ' */',
+    footer: '/* https://git.io/zixia/ */'
   },
-  sourcemap: true,
-  name: 'window',
-  banner: '/* chinese-whisper version ' + require('./package.json').version + ' */',
-  footer: '/* https://git.io/zixia/ */'
 }
